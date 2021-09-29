2a099ae11dc713e08c478dac39af4e7189908116
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-09-29 22:26:04 +0100

    styling modal

--- app/js/script.js
+++ app/js/script.js
@@ -32,3 +32,42 @@ function addBookmark() {
     bookmarked = true;
   }
 }
+
+// Modals
+const modalTriggers = document.querySelectorAll(".popup-trigger");
+const modalCloseTrigger = document.querySelector(".popup-modal__close");
+const modalContainer = document.querySelector(".modal-container");
+const bodyBlackout = document.querySelector(".body-blackout");
+
+modalTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    const { popupTrigger } = trigger.dataset;
+    const popupModal = document.querySelector(
+      `[data-popup-modal="${popupTrigger}"]`
+    );
+
+    function closeModal() {
+      console.log("Close modal");
+      popupModal.classList.remove("is--visible");
+      bodyBlackout.classList.remove("is-blacked-out");
+      modalContainer.classList.remove("is-blacked-out");
+    }
+
+    popupModal.classList.add("is--visible");
+    bodyBlackout.classList.add("is-blacked-out");
+    modalContainer.classList.add("is-blacked-out");
+
+    popupModal
+      .querySelector(".popup-modal__close")
+      .addEventListener("click", () => {
+        closeModal();
+      });
+
+    bodyBlackout.addEventListener("click", () => {
+      closeModal();
+    });
+    modalContainer.addEventListener("click", () => {
+      closeModal();
+    });
+  });
+});
