154023d9fd1f15472da76a8d07dc5fe94e89c015
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-10-01 21:50:02 +0100

    input validation

--- app/js/script.js
+++ app/js/script.js
@@ -40,6 +40,7 @@ const modalCloseTrigger = document.querySelector(".popup-modal__close");
 const bodyBlackout = document.querySelector(".body-blackout");
 const cardWrap = document.querySelectorAll(".cardWrap");
 const radios = document.querySelectorAll("input[type='radio']");
+const forms = document.querySelectorAll(".productForm form");
 
 modalTriggers.forEach((trigger) => {
   trigger.addEventListener("click", () => {
@@ -49,7 +50,8 @@ modalTriggers.forEach((trigger) => {
     );
 
     function closeModal() {
-      console.log("Close modal");
+      // Remove all error messages and inputs
+
       popupModal.classList.remove("is--visible");
       bodyBlackout.classList.remove("is-blacked-out");
     }
@@ -72,7 +74,56 @@ modalTriggers.forEach((trigger) => {
 radios.forEach((radio) =>
   radio.addEventListener("change", () => {
     cardWrap.forEach((card) => {
-      card.classList.remove("checked");
+      if (card.dataset.product == radio.dataset.product) {
+        card.classList.add("checked");
+
+        // autofocus input
+        card.querySelector("input[type='text']").focus();
+      } else {
+        card.classList.remove("checked");
+      }
     });
+  })
+);
+
+function validatePledge(form, min = 0) {
+  // Get children elements
+  let input = form.querySelector("input[type='text']");
+  let button = form.querySelector("input[type='submit']");
+  let err = form.querySelector(".errorMessage");
+
+  console.log("Validating");
+
+  if (input.value === "") {
+    form.classList.add("error");
+    err.innerText = "enter amount";
+    return;
+  } else if (input.value.match(/\D/)) {
+    form.classList.add("error");
+    err.innerText = "enter numeric characters only [0-9]";
+    return;
+  } else if (Number(input.value) < min) {
+    form.classList.add("error");
+    err.innerText = `amount must be at least $${min}`;
+    return;
+  } else {
+    form.classList.remove("error");
+    err.innerText = "";
+    return Number(input.value);
+  }
+}
+
+forms.forEach((form) =>
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    console.log("form submitted");
+    let form = e.target;
+
+    // Validate form input
+
+    validatePledge(form);
+    // Show success modal
+    // update donations
   })
 );
